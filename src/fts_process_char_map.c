c6a8a768dce8b7b4a2230f04b63181b5fcdfd6e6
HormidDev <ide-dieg@student.42madrid.com>
2024-10-23 12:08:32 +0200

    feat: Agregar flores

--- src/fts_process_char_map.c
+++ src/fts_process_char_map.c
@@ -6,7 +6,7 @@
 /*   By: ide-dieg <ide-dieg@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:43:43 by ide-dieg          #+#    #+#             */
-/*   Updated: 2024/10/21 12:44:20 by ide-dieg         ###   ########.fr       */
+/*   Updated: 2024/10/23 03:03:01 by ide-dieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ void	ft_process_char_map(t_game *game)
 {
 	int		i;
 	int		j;
+	int 	tile_pseudo_count;
+	int		tile_pseudo;
+	int		tile;
 	
 	i = 0;
 	while (i < game->map->lines)
@@ -65,6 +68,35 @@ void	ft_process_char_map(t_game *game)
 					game->map->array_content[i - 1][j] != '.')
 				game->map->array_content[i][j] = '.';
 
+			j++;
+		}
+		i++;
+	}
+	tile_pseudo_count = 0;
+	tile_pseudo = 0;
+	tile = '0';
+	i = 1;
+	while (i < game->map->lines)
+	{
+		j = 0;
+		while (j < game->map->list_content->len - 1)
+		{
+			if (game->map->array_content[i][j] == '0')
+			{
+				if (tile_pseudo_count == tile_pseudo)
+				{
+					game->map->array_content[i][j] = tile + 16;
+					tile += 5;
+					tile_pseudo_count = 0;
+					tile_pseudo++;
+					if (tile_pseudo > 4)
+						tile_pseudo = 0;
+					if (tile > '0' + 11)
+						tile -= 12;
+				}
+				else
+					tile_pseudo_count++;
+			}
 			j++;
 		}
 		i++;
